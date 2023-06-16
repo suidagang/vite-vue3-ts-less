b543f57983442cc2ae1db8bafe770c885930171a
suigang <suigang@css.com.cn>
2023-06-16 16:21:42 +0800

    test:  测试eslint会不会检测ts类型报错

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
 <script setup lang="ts">
 const num = ref<number>(1);
 num.value = 123;
-// const b: string = '111'; // 新增
-console.log(num);
+const b: number = '111'; // 新增
+console.log(num, b);
 </script>
 
 <style>
